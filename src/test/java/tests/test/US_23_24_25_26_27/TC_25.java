503f71d713105210dc109b1fa3049925969f250e
HeseNico <bckhasan@hotmail.com>
2023-06-06 21:26:32 +0200

    Hasan0606/V1

--- src/test/java/tests/test/US_23_24_25_26_27/TC_25.java
+++ src/test/java/tests/test/US_23_24_25_26_27/TC_25.java
@@ -1,24 +1,43 @@
 package tests.test.US_23_24_25_26_27;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.UserDashboard;
 import pages.UserHomepage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 public class TC_25 {
-    public static void main(String[] args) {
 
+    @Test
+    public void paymentHistory(){
+
+        // anasayfaya git
         Driver.getDriver().get(ConfigReader.getProperty("tripUrl"));
 
-
         UserHomepage userHomepage = new UserHomepage();
+        // login buttonu tikla
         userHomepage.loginLink.click();
-
+        // login email tikla ve mail yolla
         userHomepage.registrationEmailBox.sendKeys(ConfigReader.getProperty("loginEmail"));
+        // login password tikla ve password yolla
         userHomepage.registrationPasswordBox.sendKeys(ConfigReader.getProperty("loginPassword"));
+        // cikan cookies accept et
         userHomepage.cookiesAcceptButton.click();
-        userHomepage.girisLoginButtonu.click();
+        // login buttonunu tikla
+        userHomepage.loginSubmitButton.click();
+
+        UserDashboard userDashboard=new UserDashboard();
+        userDashboard.paymentHistoryButton.click();
+        // sayfada completed ara
+        String expectedIcerik="Completed";
+        String actualIcerik=userDashboard.paymentHistoryCompleted.getAttribute("innerHTML");
+
+        Assert.assertEquals(actualIcerik,expectedIcerik);
 
 
-        Driver.closeDriver();
+
+         Driver.closeDriver();
+
     }
 }
