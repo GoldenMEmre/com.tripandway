4c705b1a19bea5499a2ba7d04bbe0202b65ad611
Ömer Ünal <mrfrknl3232@gmail.com>
2023-06-06 22:12:14 +0200

    omer0606/V1

--- src/test/java/tests/test/US_18_32_33_34/US_18.java
+++ src/test/java/tests/test/US_18_32_33_34/US_18.java
@@ -2,57 +2,80 @@ package tests.test.US_18_32_33_34;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.UserHomepage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 public class US_18 {
+
+    UserHomepage userHomepage = new UserHomepage();
     @Test
     public void TC_01() {
+        //Go to https://qa.tripandway.com/
         Driver.getDriver().get(ConfigReader.getProperty("tripUrl"));
 
-        Driver.getDriver().findElement(By.xpath("//*[@type='button']")).click();  // cookies
+        // Acilan sayfada cookies kabul edilir
+        userHomepage.cookiesAcceptButton.click();
 
-        Driver.getDriver().findElement(By.xpath("//*[text()='Registration']")).click();  // registration buttonu
+        // "Registration" butonuna tiklanir
+        userHomepage.registrationLink.click();
 
-        WebElement nameRegElementi= Driver.getDriver().findElement(By.xpath("//*[@type='text'][1]"));
-        nameRegElementi.sendKeys("svd");
+        //"Name" kutusuna isim yazilir
+        userHomepage.registrationNameBox.sendKeys(ConfigReader.getProperty("loginName"));
 
-        WebElement emailRegElementi= Driver.getDriver().findElement(By.xpath("//*[@type='email']"));
-        emailRegElementi.sendKeys("svdbsk3232@gmail.com");
+        //"Email Address" kutusuna Email adresi girilir
+        userHomepage.registrationEmailBox.sendKeys(ConfigReader.getProperty("loginEmail"));
 
-        WebElement passwordRegElementi= Driver.getDriver().findElement(By.xpath("//*[@type='password']"));
-        passwordRegElementi.sendKeys("svdbsk3232@");
+        //"Password" kutusuna Pasword girilir
+        userHomepage.registrationPasswordBox.sendKeys(ConfigReader.getProperty("loginPassword"));
 
-        WebElement makeRegElementi =Driver.getDriver().findElement(By.xpath("//*[@type='submit']"));
-        makeRegElementi.click();
+        //"Make Registration" kutusuna tiklanir
+        userHomepage.registrationSubmitButton.click();
 
+        // Already Exists yazisinin gorulebildigi kontrol edilmeli
+        Assert.assertTrue(userHomepage.regAlreadyExistsMassage.isDisplayed());
+
+        // Yeni sekme acilir
+        Driver.getDriver().switchTo().newWindow(WindowType.TAB);
+
+        //Go to https://qa.tripandway.com/
+        Driver.getDriver().get(ConfigReader.getProperty("tripUrl"));
+
+        //"Login" butonuna tiklanir
+        userHomepage.loginLink.click();
+
+        // "Email Adress" kutusuna Email girilir
+        userHomepage.loginEmailBox.sendKeys(ConfigReader.getProperty("loginEmail"));
+
+        // "Password" kutusuna password girilir
+        userHomepage.loginPasswordBox.sendKeys(ConfigReader.getProperty("loginPassword"));
+
+        //"Login" butonuna tiklanir
+        userHomepage.loginSubmitButton.click();
+
+        // Title' in "Traveller Panel" oldugu kontrol edilir
+        String expectedTitle = "Traveller Panel";
+        Assert.assertEquals(expectedTitle,Driver.getDriver().getTitle());
+
+        // Close to Browser
         Driver.closeDriver();
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
-    @Test
-    public void TC_02(){
-        Driver.getDriver().get(ConfigReader.getProperty("tripUrl"));
 
-        WebElement ilkLoginElement = Driver.getDriver().findElement(By.xpath("//*[text()='Login']"));
-        ilkLoginElement.click();
-
-        WebElement emailLogginElementi = Driver.getDriver().findElement(By.xpath("//*[@type='text'][1]"));
-        emailLogginElementi.sendKeys("svdbsk3232@gmail.com");
-
-        WebElement passwordLoginElement = Driver.getDriver().findElement(By.xpath("//*[@type='password']"));
-        passwordLoginElement.sendKeys("svdbsk3232@");
-
-        WebElement ikinciLoginElementi = Driver.getDriver().findElement(By.xpath("//*[@type='submit'][1]"));
-        ikinciLoginElementi.click();
-
-        WebElement updatePasswordElement = Driver.getDriver().findElement(By.xpath("//*[text()='Update Profile']"));
-        Assert.assertTrue(updatePasswordElement.isDisplayed());
-
-        Driver.closeDriver();
-
-    }
 
 }
