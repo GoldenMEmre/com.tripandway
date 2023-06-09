5363936ac72e7b596395cd38166a8b1e785b026c
hayyam2395 <ogun.erdogan23@gmail.com>
2023-06-09 23:27:31 +0200

    US12TC140609/V3

--- src/test/java/tests/test/US_05_06_12_29/US_12.java
+++ src/test/java/tests/test/US_05_06_12_29/US_12.java
@@ -3,10 +3,7 @@ package tests.test.US_05_06_12_29;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.UserHomepage;
-import utilities.ConfigReader;
-import utilities.Driver;
-import utilities.JSUtilities;
-import utilities.ReusableMethods;
+import utilities.*;
 
 public class US_12 {
 
